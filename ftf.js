efc69cf70006b9a8fbe74bb6fb95c5f2a713758f
jkarlsen <karlsej@scc.losrios.edu>
2018-06-11 21:12:50 -0700

    Initial commit

---
+++ ftf.js
@@ -0,0 +1,52 @@
+(function() {
+	var getCookie = function(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') c = c.substring(1);
+			if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+		}
+		return "";
+	};
+	var waitForJQ = function(cb) {
+		var retrieveJQ = setTimeout(function() { // if after 6 seconds jQuery has not been loaded, load it from remote source
+			if (typeof(jQuery) === 'undefined') {
+				var a = document.createElement('script');
+				a.src = '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+				a.async = false;
+				document.getElementsByTagName('head')[0].appendChild(a);
+			}
+		}, 6000);
+		var jqCall = setInterval(function() { // ebsco warns that jquery may not be loaded initially
+			if (typeof(jQuery) === 'function') {
+				console.log('jquery found');
+				clearTimeout(retrieveJQ);
+				clearInterval(jqCall);
+				console.log('timeout cleared');
+				cb(jQuery);
+			} else {
+				console.log('no jQuery!');
+			}
+		}, 50);
+	};
+
+	waitForJQ(function($) {
+		var a = $('.ftf-results .error');
+		if (a.length) {
+			if (a.text().indexOf('No results') > -1) {
+				var college = getCookie('homeLibrary');
+				if (college !== '') {
+					college = college.toUpperCase() + ': ';
+				}
+				var citation = $('#header').text().trim();
+				if (/\w/.test(citation) === true) { // only send event if a citation is present
+					ga('send', 'event', 'full text finder', 'resource not found', college + citation);
+				}
+			}
+
+		}
+
+	});
+
+}());
