650c934ab562e0e9e3f1953cf364d5b6d598ad40
jkarlsen <karlsej@scc.losrios.edu>
2018-07-03 13:31:41 -0700

    Add meta viewport for better mobile display

--- ftf.js
+++ ftf.js
@@ -9,6 +9,7 @@ jQuery(document).ready(function($) {
 		}
 		return "";
 	};
+	$('head').append('<meta name=viewport content="width=device-width, initial-scale=1">');
 
 		var a = $('.ftf-results .error');
 		if (a.length) {
