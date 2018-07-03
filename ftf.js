232d7c55549b1f64954cba8ad859d354af62f103
jkarlsen <karlsej@scc.losrios.edu>
2018-07-03 13:32:31 -0700

    Lots of changes to default ftf behavior - too many to break out

--- ftf.js
+++ ftf.js
@@ -11,38 +11,52 @@ jQuery(document).ready(function($) {
 	};
 	$('head').append('<meta name=viewport content="width=device-width, initial-scale=1">');
 
-		var a = $('.ftf-results .error');
-		if (a.length) {
-			if (a.text().indexOf('No results') > -1) {
-				var college = getCookie('homeLibrary');
-				if (college !== '') {
-					college = college.toUpperCase() + ': ';
-				}
-				var citation = $('#header').text().trim();
-				if (/\w/.test(citation) === true) { // only send event if a citation is present
-					ga('send', 'event', 'full text finder', 'resource not found', college + citation);
-				}
-			}
-
-		}
-		else
+	var a = $('.ftf-results .error');
+	if (a.length)
+	{
+		if (a.text().indexOf('No results') > -1)
 		{
+			var college = getCookie('homeLibrary');
+			if (college !== '')
+			{
+				college = college.toUpperCase() + ': ';
+			}
+			var citation = $('#header').text().trim();
+			if (/\w/.test(citation) === true)
+			{ // only send event if a citation is present
+				ga('send', 'event', 'full text finder', 'resource not found', college + citation);
+			}
+		}
+
+	}
+	else
+	{
+		$('a').removeAttr('target'); // have articles open in same window; usually this page is arrived at as a new window
+		var cookieID = 'lrFTF_' + encodeURIComponent($('.page-title').text().slice(0,20)); // need to set a unique id -- no accession number on page so use first bit of title
+		if (getCookie(cookieID) !== '1') // if user hits back button, the following will not happen.
+		{
+
 			// send user on to first link if it is a full-text link - set cookie to allow back button to be used in case of error
 			var firstLink = $('a[data-auto="menu-link"]:first'); // get first link
 			if (firstLink.closest('.basic-list').prev().text().indexOf('Full Text') > -1) // our links are in categories--makes sure this is under the correct label
 			{
 				var destination = firstLink.attr('href');
-				var cookieID = 'lrFTF_' + destination.slice(-20); // need to set a unique id -- no accession number on page so use end of link
-				if (getCookie(cookieID) !== '1') // if user hits back button, the following will not happen
-				{
-					document.cookie = cookieID + '=1'; // set cookie to allow back button to function
-					var url = location.protocol + '//' + location.hostname + destination; // href attribute includes relative path so need to construct full URL
+				document.cookie = cookieID + '=1'; // set session-only cookie to allow back button to function
+				firstLink.parent().append('<span style="display:none;" class="redirecting" role="alert">Redirecting... <img id="loader" alt="loading" src="//www.library.losrios.edu/resources/databases/loader.gif"></span>');
+				$('.redirecting').fadeIn('slow');
+				var url = location.protocol + '//' + location.hostname + destination; // href attribute includes relative path so need to construct full URL
+				var fallBack = setTimeout(function() { // if for some reason image never loads
 					location.href = url;
-				}
+				}, 3000);
+				$('#loader').on('load', function() { // appears to be the only way to get loader gif to show
+					clearTimeout(fallBack);
+					location.href = url;
+				});
+				
+				
 			}
 		}
-	});
-
+	}
 	
 	
 });
