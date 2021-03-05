5e5a75947eedb786caced3fa55b7593af51b1a12
jkarlsen <karlsej@scc.losrios.edu>
2021-03-05 15:37:15 -0800

    Remove custom DOI/unpaywall check
    Relied upon EDS API

--- ehost-scripts.js
+++ ehost-scripts.js
@@ -10,92 +10,7 @@
 			}
 		document.cookie = cname + '=' + cvalue + ';' + expires + ' path=/; domain = ' + domain;
 	};
-var getDOI = function(db, an, container, edsGW) { // get doi from api. This is better than getting it from page because many records do not show it on the page but the info is there in the API. 
-	//     console.log('getDOI running');
-	if (db !== '') {
-		var doi = '';
-		var URL = 'https://widgets.ebscohost.com/prod/encryptedkey/eds/eds.php?k=' + edsGW + '&s=0,1,1,0,0,0&q=';
-		var query = 'retrieve?dbid=' + db + '&an=' + an;
-		var retrieveURL = URL + encodeURIComponent(query);
-		jQuery.getJSON(retrieveURL)
-			.done(function(data) {
-				//              console.log(data);
-				var identifiers = data.Record.RecordInfo.BibRecord.BibEntity.Identifiers;
-				if (identifiers) {
-					jQuery.each(identifiers, function(i) {
-						if (identifiers[i].Type === 'doi') {
-							doi = identifiers[i].Value;
-							//                      console.log(doi);
-							getOADoi(doi, container);
-						}
-					});
-				}
-			})
-			.fail(function(a, b, c) {
-				console.log('eds api lookup failed, error: ' + c);
-			});
 
-	}
-};
-
-var getOADoi = function(doi, container) { // use oadoi api to look for free full text.
-	if (doi !== '') {
-		var email = 'karlsej@scc.losrios.edu'; // supposed to append this to query url to help them track usage
-		var apiURL = 'https://api.unpaywall.org/v2/' + doi + '?email=' + email;
-		jQuery.getJSON(apiURL).done(function(data) {
-			var isOA = data.best_oa_location;
-			//         console.log('oadoi url: ' + url);
-			if (isOA !== null) {
-				console.log('OA doi found');
-				var url = isOA.url;
-				appendOALink(url, container);
-			}
-
-		}).fail(function(a, b, c) {
-			console.log('oadoi link error: '+ c);
-		});
-	}
-
-};
-
-var appendOALink = function(url, container) {
-	//     console.log(container);
-	var el;
-	if (container.hasClass('display-info')) {
-		el = jQuery('<div />');
-	} else if (container.hasClass('format-control')) {
-		el = jQuery('<li />');
-	}
-	//     console.log(el);
-	el.css('border', 'none').addClass('custom-link-item').html('<span class="custom-link"><a target="_blank" href="' + url + '"><img class="icon-image" src="//www.library.losrios.edu/resources/link-icons/oa.png" alt="">View Full Text (open access)</a></span>').appendTo(container);
-};
-
-var getItemData = function(type, el) {
-	var info;
-	if (type === 'detail') {
-		return {
-			db: ep.clientData.currentRecord.Db,
-			an: ep.clientData.currentRecord.Term
-		};
-	} else if (type === 'result') {
-		info = el.find('em.preview-hover').data('hoverpreviewjson');
-		if (info !== undefined) {
-			return {
-				db: info.db,
-				an: info.term
-			};
-			//           console.log('Result ' + (i + 1) + ': db is ' + db + '  and AN is ' + an);
-
-		} else {
-			return {
-				db: '',
-				an: ''
-			};
-		}
-	} else {
-		return undefined;
-	}
-};
 var smallScreen = (function()
 {
 	if (document.documentElement.clientWidth < 1024)
@@ -142,28 +57,24 @@ var jQCheck = setInterval(function() {
 				'custID': 'amerriv',
 				'abbr': 'arc',
 				'homepage': 'http://www.arc.losriso.edu/arclibrary/',
-				'edsGW': 'eyJjdCI6IllDeGlaa21JbEU2VlRmU1FIUmk3XC94Nk9ZNnN4RHRkXC9Hb2NKbXJ4OVo0bz0iLCJpdiI6IjZiZDA4YWI4N2IwYjgwOWIxMTI0ZmEwNDYzYWQxOGQ0IiwicyI6ImE4MGQ5MDk4ZjZmYjM5MjMifQ==&p=YW1lcnJpdi5tYWluLndzYXBp'
 			},
 			{
 				'colName': 'Folsom Lake College',
 				'custID': 'ns015092',
 				'abbr': 'flc',
 				'homepage': 'https://www.flc.losrios.edu/libraries/',
-				'edsGW': 'eyJjdCI6InVmNWlcL2tIU3BkNFJWXC9iaU9LTTlhSW1sUmtDR0lUbjBBWTczampFdjFsZz0iLCJpdiI6IjliMjBmMGNmODk5NjJiY2Q1ZTdiZTExMjVlN2QzMmZmIiwicyI6ImIwNzk5OWNiYTI1NTU1NmMifQ==&p=bnMwMTUwOTIubWFpbi53c2FwaQ=='
 			},
 			{
 				'colName': 'Cosumnes River College',
 				'custID': 'cosum',
 				'abbr': 'crc',
 				'homepage': 'https://www.crc.losrios.edu/services/library/',
-				'edsGW': 'eyJjdCI6IjNLNXdlQ0I2V09TWDdBUkYzT3cyaVE9PSIsIml2IjoiOTJkYzAwNzNhN2M4MWE1NDY1ZTg4ZjEzNTFmNjdiYjQiLCJzIjoiZThjNzZmYWE3NDc4ZTM2MiJ9&p=Y29zdW0ubWFpbi53c2FwaQ=='
 			},
 			{
 				'colName': 'Sacramento City College',
 				'custID': 'sacram',
 				'abbr': 'scc',
 				'homepage': 'https://www.scc.losrios.edu/library/',
-				'edsGW': 'eyJjdCI6Im9GbVUzeldxZzg3ZzhXelNKYktRbmc9PSIsIml2IjoiYzg3NWRhOWRhOTQ5ZjkxMGIxZGFiOTAwZDJkZmJhYTUiLCJzIjoiZTAxZTQzYmZhOTViYjJlZSJ9&p=c2FjcmFtLm1haW4ud3NhcGk='
 			}
 
 		];
@@ -186,23 +97,7 @@ var jQCheck = setInterval(function() {
 			
 		//  var college = document.getElementById('collegeID');
 
-		if (location.href.indexOf('/results?') > -1) {
-			$('.result-list-record').each(function() { // initiate oadoi search
-				var a = $(this);
-				if (a.find('.pubtype-icon').attr('title') === 'Academic Journal') {
-					var custLinks = a.find('.externalLinks');
-					var linkText = custLinks.text();
-					if (linkText.indexOf('Full Text') > -1) {} else {
-						var info = getItemData('result', a);
-						if (info) {
-							// console.log(info.db);
-							// console.log(info.an);
-							getDOI(info.db, info.an, $(this).find('.display-info'), edsGW);
-						}
-					}
-				}
-			});
-		} else if (location.href.indexOf('/detail/') > -1) {
+		if (location.href.indexOf('/detail/') > -1) {
 			(function()
 			{
 				// permalinks on detailed record pages
@@ -269,28 +164,6 @@ var jQCheck = setInterval(function() {
 
 
 			}());
-
-			var info = getItemData('detail', null);
-			var db = info.db;
-			var an = info.an;
-			var article = false;
-			var pubType = $('dt:contains("Publication Type")');
-			var docType = $('dt:contains("Document Type")');
-			if (pubType.length) {
-				if (pubType.next().text().indexOf('Journal') > -1) {
-					article = true;
-				}
-			} else if ((docType).length) {
-				if (docType.next().text().indexOf('Article') > -1) {
-					article = true;
-				}
-			}
-			if (article === true) {
-				if ((!($('.custom-link-item a:contains("Full Text")').length)) && (!($('.pdf-ft').length)) && (!($('.html-ft').length)) ) { // look for free full text via oadoi
-					//      console.log('db: ' + db + ', an: ' + an);
-					getDOI(db, an, $('.format-control:first-of-type'), edsGW);
-				}
-			}
 
 		}
 		if (smallScreen)
