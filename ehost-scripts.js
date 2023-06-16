0a06816784179df9d408cb74ab48578d498667d2
jkarlsen <karlsej@scc.losrios.edu>
2023-06-15 23:20:23 -0700

    Change syntax to ES6

--- ehost-scripts.js
+++ ehost-scripts.js
@@ -1,17 +1,17 @@
-(function() {
-	var setCookie = function(cname, cvalue, exdays, domain) {
-		var expires; // set exdays to false for session-only cookie
+(() => {
+	const setCookie = (cname, cvalue, exdays, domain) => {
+		let expires; // set exdays to false for session-only cookie
 		if (exdays !== false) {
-			var d = new Date();
+			const d = new Date();
 			d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
 			expires = ' expires=' + d.toUTCString() + ';';
 			} else {
 				expires = '';
 			}
-		document.cookie = cname + '=' + cvalue + ';' + expires + ' path=/; domain = ' + domain;
+		document.cookie = `${cname}=${cvalue};${expires} path=/; domain = ${domain}`;
 	};
 
-var smallScreen = (function()
+const smallScreen = (() =>
 {
 	if (document.documentElement.clientWidth < 1024)
 	{
@@ -21,22 +21,22 @@ var smallScreen = (function()
 	{
 		return false;
 	}
-}());
+})();
 // show note when there are problems
-var showNote = (function(fileName) {
-	if (document.cookie.indexOf(fileName + '=hide') === -1) {
-		var url = 'https://www.library.losrios.edu/resources/ehost-scripts/' + fileName + '.php'; // this file needs to be edited with current message
+const showNote = (fileName) => {
+	if (document.cookie.indexOf(`${fileName}=hide`) === -1) {
+		const url = `https://www.library.losrios.edu/resources/ehost-scripts/${fileName}.php`; // this file needs to be edited with current message
 		$.get(url)
 		.done(function(response) {
 			if (response !== '') {
-				$('<p id="problem-note" style="display:none;">' + response + '<button id="problem-note-dismiss" class="button" type="button">Hide this message</button></p>').prependTo('#header').slideDown('slow');
+				$(`<p id="problem-note" style="display:none;">${response}<button id="problem-note-dismiss" class="button" type="button">Hide this message</button></p>`).prependTo('#header').slideDown('slow');
 				$('#problem-note-dismiss').on('click', function() {
 					$(this).parent().slideUp();
-					var domain = (function() {
-						var arr = location.host.split('.');
-						var result = arr.slice(arr.length -2);
+					const domain = (() => {
+						const arr = location.host.split('.');
+						const result = arr.slice(arr.length -2);
 						return result.join('.');
-						}());
+						})();
 					setCookie(fileName, 'hide', false, domain);
 					});
 				}
@@ -45,14 +45,14 @@ var showNote = (function(fileName) {
 			console.log('problem note error: ' + c);
 		});
 		}
-});
-var jQCheck = setInterval(function() {
+};
+const jQCheck = setInterval(() => {
 	if (typeof(jQuery) === 'function')  {
 		clearInterval(jQCheck);
-		var $ = jQuery;
-		var pid = ep.clientData.pid.split('.');
-		var custID = pid[0];
-		var college = [{ // allow use of different API profiles for sake of stats
+		const $ = jQuery;
+		const pid = ep.clientData.pid.split('.');
+		const custID = pid[0];
+		const colleges = [{ // allow use of different API profiles for sake of stats
 				colName: 'American River College',
 				custID: 'amerriv',
 				abbr: 'arc',
@@ -90,29 +90,29 @@ var jQCheck = setInterval(function() {
 			}
 
 		];
-		var abbr;
-		var currentCol;
-		var libchatHash;
-		var libchatImage = '';
-		for (var i = 0; i < college.length; i++) { // set api profile
-			if (college[i].custID === custID) {
-				abbr = college[i].abbr;
-				currentCol = college[i];
-				libchatHash = college[i].libchatHash;
-				libchatImage = college[i].libchatImage;
-				imgHeight = college[i].imgHeight;
+		let abbr;
+		let currentCol;
+		let libchatHash;
+		let libchatImage = '';
+		for (let col of colleges) { // set api profile
+			if (col.custID === custID) {
+				abbr = col.abbr;
+				currentCol = col;
+				libchatHash = col.libchatHash;
+				libchatImage = col.libchatImage;
+				imgHeight = col.imgHeight;
 			}
 		}
-		var domain = 'ebscohost.com';
+		let domain = 'ebscohost.com';
 		if (location.hostname.indexOf('losrios.edu') > -1) {
 			setCookie('onesearchDomain', 'proxy', false, 'losrios.edu');
 			domain = 'losrios.edu';
 		}
 		setCookie('homeLibrary', abbr, 30, domain);
-		var proxy = 'https://ezproxy.losrios.edu/login?url=';
-		var homePage = proxy + 'search.ebscohost.com/login.aspx?authtype=cookie,ip,uid&profile=' + ep.interfaceId + '&defaultdb=' + ep.clientData.db[0];
-		var permalink = '';
-		var pageType = '';
+		const proxy = 'https://ezproxy.losrios.edu/login?url=';
+		const homePage = `${proxy}search.ebscohost.com/login.aspx?authtype=cookie,ip,uid&profile=${ep.interfaceId}&defaultdb=${ep.clientData.db[0]}`;
+		let permalink = '';
+		let pageType = '';
 		if (location.href.indexOf('/basic') > -1) {
 			permalink = homePage;
 			pageType = 'basic';
@@ -124,7 +124,7 @@ var jQCheck = setInterval(function() {
 				permalink = $('#pLink').val();
 			}
 			pageType = 'result';
-		} else if (location.href.indexOf('/detail/') > -1) {
+		} else if (location.href.indexOf('ehost/detail') > -1) {
 			permalink = ep.clientData.plink;
 			pageType = 'detailedRecord';
 		} else if (location.href.indexOf('/advanced') > -1) {
@@ -135,15 +135,15 @@ var jQCheck = setInterval(function() {
 		//  var college = document.getElementById('collegeID');
 		if (pageType === 'detailedRecord') {
 			
-			(function()
+			(() =>
 			{
 				// permalinks on detailed record pages
 				$('.citation-title').after(
-					'<dt>Permalink:</dt><dd id="lr-permalink-dd"><input id="lr-input-permalink" type="text" value="' + permalink + '"> <button id="lr-copy-permalink" type="button" class="button ">&#128279; Copy URL</button></dd>'
+					`<dt>Permalink:</dt><dd id="lr-permalink-dd"><input id="lr-input-permalink" type="text" value="${permalink}"> <button id="lr-copy-permalink" type="button" class="button ">&#128279; Copy URL</button></dd>`
 
 				);
 
-				var input = $('#lr-input-permalink');
+				const input = $('#lr-input-permalink');
 				input.on('click', function()
 				{
 					this.select();
@@ -161,7 +161,7 @@ var jQCheck = setInterval(function() {
 						{
 							$('body').append('<div id="lr-permalink-copied" >Link copied to clipboard</div>');
 						}
-						var permCopied = $('#lr-permalink-copied');
+						const permCopied = $('#lr-permalink-copied');
 						permCopied.dialog(
 						{
 							create: function()
@@ -200,53 +200,47 @@ var jQCheck = setInterval(function() {
 
 
 
-			}());
+			})();
 
 		}
 		if (smallScreen)
 		{
-			$('#footerLinks').prepend('<li><a href="' + currentCol.homepage + '">' + currentCol.colName + ' Library</a></li>');
+			$('#footerLinks').prepend(`<li><a href="${currentCol.homepage}">${currentCol.colName} Library</a></li>`);
 
 		}
 
-			}, 100);
-		};
-
-		$('.email-link').on('click', function()
-		{
-			loadSubj('#DeliveryEmailSubject');
-		});
+		
 		// add problem report button
 		if ($('.format-control').length) {
 			$('#Column1Content').append('<button class="button" type="button" id="lr-problem-reporter">Report a problem</button>');
 			$('#lr-problem-reporter').on('click', function() {
-				var w = 600;
-				var h = 600;
-				var left = (screen.width - w) / 2;
-				var top = (screen.height - h) / 4;
-				var refUrl = ep.clientData.plink || '';
-				var itemID = ep.clientData.currentRecord.Term || '';
-				window.open('https://library.losrios.edu/utilities/problem-reporter/?url=' + encodeURIComponent(refUrl) + '&recordid=' + itemID + '&college=' + currentCol.abbr + '&source=ebsco', 'Problem reporter', 'toolbar=no, location=no, menubar=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+				const w = 600;
+				const h = 600;
+				const left = (screen.width - w) / 2;
+				const top = (screen.height - h) / 4;
+				const refUrl = ep.clientData.plink || '';
+				const itemID = ep.clientData.currentRecord.Term || '';
+				window.open(`https://library.losrios.edu/utilities/problem-reporter/?url=${encodeURIComponent(refUrl)}&recordid=${itemID}&college=${currentCol.abbr}&source=ebsco`, 'Problem reporter', 'toolbar=no, location=no, menubar=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
 
 
 			});
 		}
 		
 		showNote('note');
-		(function() { // load libchat
-			var div = document.createElement('div');
+		(() => { // load libchat
+			const div = document.createElement('div');
 			div.id = 'libchat_' + libchatHash;
 			document.getElementsByTagName('body')[0].appendChild(div);
-			var chatWinWidth = 300;
-			var chatWinHeight = 340;
-			var chatUrl = 'https://answers.library.losrios.edu/chat/widget/' + libchatHash + '?referer=' + encodeURIComponent(permalink);
-			$(div).append('<a id="libchat-popup" href="' + chatUrl + '"><img style="height:' + imgHeight + ';" class="libchat_btn_img"  src="' + libchatImage + '" alt="Ask a Librarian"></a>');
+			const chatWinWidth = 300;
+			const chatWinHeight = 340;
+			const chatUrl = `https://answers.library.losrios.edu/chat/widget/${libchatHash}?referer=${encodeURIComponent(permalink)}`;
+			$(div).append(`<a id="libchat-popup" href="${chatUrl}"><img style="height:${imgHeight};" class="libchat_btn_img"  src="${libchatImage}" alt="Ask a Librarian"></a>`);
 			$('#libchat-popup').on('click', function(e) {
 				e.preventDefault();
 				window.open(chatUrl, 'libchat', 'height=340,width=300,menubar=no,statusbar=no,left=' + ($(window).width() - chatWinWidth - 20) + ',top=' + ($(window).height() - chatWinHeight - 150));
 			});
-		}());		
+		})();		
 	}
 }, 100);
 	
-}());
+})();
